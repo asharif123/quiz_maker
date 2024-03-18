74876215c79651ec47ac60080e969bfd64f29ec3
asharif123 <awadsharif9@gmail.com>
2024-03-17 22:03:22 -0700

    added comments describing the project

--- Class1.cs
+++ Class1.cs
@@ -1,4 +1,26 @@
-﻿namespace quiz_maker
+﻿/*using System.Collections.Generic;
+
+What program has multiple choices, random questions and user scoring? 
+A quiz maker program! 
+Design a program which asks the user for a list of questions, multiple choice answers for each question 
+and the ability to specify which answer(s) are right. 
+The program will then add those questions and answers to a list that gets stored on your harddisk using 
+serialisation.The program can then read that file, randomly pick a question and load up its answers 
+for the user to choose from. When the user chooses an answer the program will determine if the answer chosen is correct.
+It should also keep score for the end.
+
+rocket Tips: First we need to create our repository of questions and answers. 
+A simple way to do this is to have your program open up and ask the user to enter questions and their answers. 
+Each question and the answers they add will be stored in a file (serialisation is your friend).
+
+An example might be “What color is the sky?” and list “red, blue, green” as the answers. 
+The answer “Blue” is marked as the right answer (which you obviously don’t show the user taking the quiz).
+When the user selects the answer it gets compared it to the correct one stored.
+Each successful answer can be added to a counter variable. 
+At the end of the quiz their score is this counter variable over the number of questions asked.
+*/
+
+namespace quiz_maker
 {
     public class Class1
     {
