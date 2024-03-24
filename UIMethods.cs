e79bb51e997636cb51efb8ebc26773ce501546ea
asharif123 <awadsharif9@gmail.com>
2024-03-24 13:24:06 -0700

    added tryparse method in AskNumberOfQuestions method

--- UIMethods.cs
+++ UIMethods.cs
@@ -6,19 +6,34 @@
         {
             Console.WriteLine("\nWelcome to Quiz Maker!\n");
         }
-//method to return number of questions inputted by user
+        //method to return number of questions inputted by user
         public static int AskNumberOfQuestions()
         {
-            Console.WriteLine("How many questions would you like to answer?");
-            int numberOfQuestions;
-    //expect user to input integer value
-            Console.ReadLine();
-            numberOfQuestions = Convert.ToInt32(Console.ReadLine());
+            //ensure user inputs correct value
+            bool notValidInput = true;
+            int numberOfQuestions = 0;
+
+            while (notValidInput)
+            {
+                Console.WriteLine("How many questions would you like to answer?");
+                //expect user to input integer value
+                string questionsToInput = Console.ReadLine();
+                bool isValid = int.TryParse(questionsToInput, out numberOfQuestions);
+
+                if (!isValid)
+                {
+                    Console.WriteLine("Please enter a valid integer!\n");
+                }
+                else
+                {
+                    notValidInput = false;
+                }
+            }
             return numberOfQuestions;
         }
 
-     //take questions user has answered and add them to list
-     public static string InputQuestions()
+        //take questions user has answered and add them to list
+        public static string InputQuestions()
         {
             Console.WriteLine("Please input your question!\n");
             string inputQuestions = Console.ReadLine();
