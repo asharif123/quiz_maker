a7519a310455d5fff9be01a6abe814804c085e55
asharif123 <awadsharif9@gmail.com>
2024-04-07 16:30:10 -0700

    changed code around in Program.cs file

--- Program.cs
+++ Program.cs
@@ -26,16 +26,17 @@ namespace quiz_maker
                 //use List<Quiz> type since you are showing a list of questions stored as classes
                 List<Quiz> totalListOfQuestions = UIMethods.CreateListOfQuestions();
 
-                //randomly select a question of Quiz (name class) type
-                //pass random rd as an argument
-                Quiz randomQuestion = UIMethods.RandomlySelectedQuestion(totalListOfQuestions, rd);
 
                 //serialization on the entire list of Questions user has inputted
                 //take in a list of quizzes
                 List<Quiz> serializedQuestions = Logic.SaveQuizOnXML(totalListOfQuestions);
 
                 //deserialization
-                List<Quiz> deserializedQuestions = Logic.LoadQuizOnXml(totalListOfQuestions);
+                List<Quiz> deserializedQuestions = Logic.LoadQuizOnXml(serializedQuestions);
+
+                //randomly select a question of Quiz (name class) type
+                //pass random rd as an argument
+                Quiz randomQuestion = UIMethods.RandomlySelectedQuestion(deserializedQuestions, rd);
 
                 if (UIMethods.AskUserToPlayAgain() == false)
                 {
