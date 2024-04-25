08b59153fc6d3690082fe6c0f080734b76bab988
asharif123 <awadsharif9@gmail.com>
2024-04-24 22:38:29 -0700

    updated comments in Constants.cs file

--- Constants.cs
+++ Constants.cs
@@ -7,8 +7,8 @@ namespace quiz_maker
         public const char NEW_QUIZ = 'c';
         public const char CREATE_QUIZ = 'r';
         public const char CONTINUE_PLAYING = 'y';
-       //user can enter 4 answers per question
         public const int MIN_ANSWERS = 1;
+        //user can enter 4 answers per question
         public const int MAX_ANSWERS = 4;
         //score to increase user's score
         public const int INCREMENT_SCORE = 5;
