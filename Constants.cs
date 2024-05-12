e831cfa8f7ac03b31d0fa84e99abff72cf3605da
asharif123 <awadsharif9@gmail.com>
2024-05-12 10:34:04 -0700

    updated comments in Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -6,10 +6,12 @@ namespace quiz_maker
     {
         public const char NEW_QUIZ = 'c';
         public const char CONTINUE_PLAYING = 'y';
+        //user needs to enter a question having at least 10 characters in it
         public const int MIN_QUESTION_LENGTH = 10;
         public const int MIN_QUESTIONS = 1;
         public const int MAX_QUESTIONS = 1000;
         public const int MIN_ANSWER_LENGTH = 1;
+        //user needs to enter a question having at least 1 character
         public const int MIN_ANSWERS = 1;
         //user can enter 4 answers per question
         public const int MAX_ANSWERS = 4;
